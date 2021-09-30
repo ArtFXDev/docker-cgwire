200d571f6cea42940d4da218d5b44dcb19ee5ce8
Simon Lambin <slambin@artfx.fr>
2021-09-30 13:17:54 +0200

    Clone zou from the master branch of the artfx repository

--- zou/init_zou.sh
+++ zou/init_zou.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
-
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 mkdir -p ${PREVIEW_FOLDER} ${TMP_DIR}
 
-zou upgrade_db
-zou init_data
-zou create_admin admin@example.com --password mysecretpassword
+zou upgrade-db
+zou init-data
+zou create-admin admin@example.com --password mysecretpassword
